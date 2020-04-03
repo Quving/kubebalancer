e5a9ddb35aa8e0485988d1814e1aea057e02717c
Vinh Ngu <vinh-ngu@hotmail.com>
2020-04-03 19:10:51 +0200

    Add example to restart rollout and watch the progress.

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import subprocess
 import time
 
 from kubernetes import client, config
@@ -27,9 +28,10 @@ class KubeApi:
         for r in res.items:
             print(r.metadata.name)
 
-    def wait_for_deployment_complete(self, deployment_name, namespace, timeout=60):
+    def wait_for_deployment_complete(self, deployment_name, namespace, timeout=180):
         api = client.AppsV1Api()
         start = time.time()
+        msg = ''
         while time.time() - start < timeout:
             time.sleep(2)
             response = api.read_namespaced_deployment_status(deployment_name, namespace)
@@ -40,15 +42,34 @@ class KubeApi:
                     s.observed_generation >= response.metadata.generation):
                 return True
             else:
-                print(f'[updated_replicas:{s.updated_replicas},replicas:{s.replicas}'
-                      ',available_replicas:{s.available_replicas},observed_generation:{s.observed_generation}] waiting...')
+                msg_new = '[updated_replicas: {}, replicas: {}, available_replicas: {}, observed_generation: {}]' \
+                    .format(s.updated_replicas, s.replicas - 1, s.available_replicas, s.observed_generation)
+                if not msg == msg_new:
+                    print(msg_new)
+                    msg = msg_new
 
         raise RuntimeError(f'Waiting timeout for deployment {deployment_name}')
 
-    # def restart_rollout(self, deployment_name, namespace):
+    def excute_shell_cmd(self, cmd_str):
+        process = subprocess.Popen(cmd_str.split(), stdout=subprocess.PIPE, universal_newlines=True)
+        while True:
+            output = process.stdout.readline()
+            print(output.strip())
+            # Do something else
+            return_code = process.poll()
+            if return_code is not None:
+                # Process has finished, read rest of the output
+                for output in process.stdout.readlines():
+                    print(output.strip())
+                return return_code
+
+    def restart_rollout(self, deployment_name, namespace):
+        command = 'kubectl rollout restart deployment/{} -n {}'.format(deployment_name, namespace)
+        self.excute_shell_cmd(command)
 
 
 if __name__ == '__main__':
     kubeapi = KubeApi()
     # kubeapi.get_deployments(namespace='testing')
+    kubeapi.restart_rollout('eos-web', 'testing')
     kubeapi.wait_for_deployment_complete('eos-web', 'testing')
